1254185d7ccebd25517e76be0eaf1f99d7519c96
Nelson Mejia <nelsonjaviermejialemus234@gmail.com>
2025-10-07 18:35:26 -0600

    codigo comentado

--- grafica-Lineas.py
+++ grafica-Lineas.py
@@ -1,15 +1,21 @@
+#importamos la librería
 import matplotlib.pyplot as plt
 
-# Datos
+# Lista de los 10 nombres de los compañeros 
 nombres = ['Eliseo', 'Roberto', 'Andrea', 'Alejandro', 'Ricardo','Alberto','Bryan', 'Adaly','Omar','Antonio']
+
+#Lista con las horas que cada compañero durmió anoche
 horas_dormidas = [5, 8, 7, 5, 7, 7, 4, 8, 3, 5]
 
-# Crear gráfica de barras
+# Crear gráfica de barras usando los nombres y las horas dormidas --- cada barra representa,
+# cuántas horas durmió una persona
 plt.bar(nombres, horas_dormidas, color='skyblue')
 
-# Título y etiquetas
+# Le agregar un titulo a la grafica
 plt.title('¿Cuántas horas dormiste anoche?')
+
+# Le agregamos una etiqueta al eje vertical Y
 plt.ylabel('Horas dormidas')
 
-# Mostrar gráfica
+# Mostramos la grafica 
 plt.show()
